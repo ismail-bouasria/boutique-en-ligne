abe46db0a59d69a3b6a64aa994253f1e8baeba3e
ismail <ismail.bouasria@laplateforme.io>
2022-07-14 17:39:36 +0200

    update header
    correction de bug nav

--- php/accueil.php
+++ php/accueil.php
@@ -17,6 +17,7 @@ require '../classes/Panier.php';
     <link rel="stylesheet" href="../assets/cssboot/bootstrap.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="../js/bootstrap.js"></script>
+    <script src="https://kit.fontawesome.com/53bdaa6800.js" crossorigin="anonymous"></script>
 </head>
 
 <body class="w-full h-full">
--- require/header.php
+++ require/header.php
@@ -1,7 +1,7 @@
 <header>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><img src="../assets/img/nwfe-logo.png" width="100" height="100" alt=""></a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light bg-white">
+  <a class="navbar-brand" href="accueil.php"><img src="../assets/img/nwfe-logo.png" width="100" height="100" alt=""></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -9,7 +9,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="mr-1 nav-item active">
-        <a class="nav-link " href="#">Accueil <span class="sr-only">(current)</span></a>
+        <a class="nav-link " href="accueil.php">Accueil <span class="sr-only">(current)</span></a>
       </li>
       <li class=" mr-5 nav-item dropdown">
         <a class="  nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -71,8 +71,8 @@
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <input class="form-control mr-sm-3" type="search" placeholder="Chercher votre produit" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Chercher</button>
     </form>
   </div>
 </nav>
