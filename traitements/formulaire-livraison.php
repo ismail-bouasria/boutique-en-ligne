d9dc9f504d8b3b3e9788b8d65a3f9327dae80567
ismail <ismail.bouasria@laplateforme.io>
2022-07-25 15:00:00 +0200

    update mode-livraison

--- traitements/formulaire-livraison.php
+++ traitements/formulaire-livraison.php
@@ -13,19 +13,21 @@ if (isset($_POST['click'])) {
     $idAdress = 1;
     $idUser= $_SESSION['id'];
     $commande->UpdateAdressCommande($idAdress,$idUser);
+    header('Location:../php/paiement.php');
     
 }elseif (isset($_POST['place'])) {
 
     $idAdress = 2;
     $idUser= $_SESSION['id'];
     $commande->UpdateAdressCommande($idAdress,$idUser);
+    header('Location:../php/paiement.php');
 
 }elseif (isset($_POST['livraison'])) {
     
     $idUser= $_SESSION['id'];
     $idAdress = $Adress->selectIdAdressByUser($idUser);
-   
     $commande->UpdateAdressCommande($idAdress,$idUser);
+    header('Location:../php/paiement.php');
 }
 
 ?>
