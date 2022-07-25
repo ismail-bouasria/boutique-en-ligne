643e683ac9569b4c5b2c6ecb3821928453535422
ismail <ismail.bouasria@laplateforme.io>
2022-07-25 15:40:39 +0200

    responsiv page produit finish

--- php/page-produit.php
+++ php/page-produit.php
@@ -109,7 +109,7 @@ $_SESSION['idproduit'] = $infosProduit['id'];
                                     <h1> <?php echo $valueP['nom']; ?></h1>
                                     <p> <?php echo $valueP['description']; ?></p>
                                     <h1> Prix : <span> <?php echo $valueP['prix']; ?>€</span> </h1>
-                                    <a href="page-produit.php?produit=<?php echo $valueP['id']; ?>" class="btn btn-primary" target="blank"> Commander !</a>
+                                    <a href="page-produit.php?produit=<?php echo $valueP['id']; ?>" class="btn btn-primary"> Commander !</a>
                                 </div>
                             </div>
                         
